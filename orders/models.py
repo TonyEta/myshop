693e278b9c3c0ccfdd8df809ae0ca7a4b942088f
TontEta <shamruk01@gmail.com>
2026-07-06 17:07:09 +0300

    fix: fix and upgrade previous migrations

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from products.models import Product
+
 
 class Order(models.Model):
     STATUS_CHOICES = (
@@ -16,7 +18,24 @@ class Order(models.Model):
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     shipping_address = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updatet_at = models.DateTimeFiedl(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.user}-{self.status}-{self.total_price}"
+
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.product.name}-total_sum-{self.quantity * self.price}"
+    
+    def save(self, *args, **kwargs):
+        if not self.price:
+            self.price = self.product.price
+
+        super().save(*args, **kwargs)
