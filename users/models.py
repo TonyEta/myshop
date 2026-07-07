97683b2b5fe9caa5267c5a68fd57e48adcadbead
TontEta <shamruk01@gmail.com>
2026-07-07 13:06:34 +0300

    add Profile model for users app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    phone_number = models.
+    date_of_birth = models.DateField()
+    address = models.TextField()
+
+    def __str__(self):
+        return f"{self.user.username} profile"
