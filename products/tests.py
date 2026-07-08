dcc6c4b6056520fec9d189899c5aa51bb57d6c3d
TontEta <shamruk01@gmail.com>
2026-07-08 11:22:56 +0300

    add home & '' path to global ulrs and products.urls and create basic view for this path \ write search and filter logic to queryset

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import RequestFactory, TestCase
 
-# Create your tests here.
+from .models import Category, Product
+from .views import ProductList
+
+
+class ProductListTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.category = Category.objects.create(name='Malt', slug='malt')
+        self.product = Product.objects.create(
+            name='Pilsner Malt',
+            description='Test product',
+            price=9.99,
+            category=self.category,
+            image=SimpleUploadedFile(
+                'test.jpg',
+                b'fake-image-bytes',
+                content_type='image/jpeg',
+            ),
+        )
+
+    def test_filters_products_by_category_name(self):
+        request = self.factory.get('/products/', {'category': 'Malt'})
+        view = ProductList()
+        view.setup(request=request)
+
+        queryset = view.get_queryset()
+
+        self.assertIn(self.product, queryset)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.db.models import Q
 
-# Create your views here.
+from .models import Product
+
+
+class ProductList(ListView):
+    model = Product
+    template_name = 'products/home-products-list.html'
+    context_object_name = 'products'
+    paginate_by = 12
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        category = self.request.GET.get('category')
+        price_min = self.request.GET.get('price_min')
+        price_max = self.request.GET.get('price_max')
+        search = self.request.GET.get('search')
+
+
+        if category:
+            queryset = queryset.filter(category__name=category)
+        if price_min:
+            queryset = queryset.filter(price__gte=price_min)
+        if price_max:
+            queryset = queryset.filter(price__lte=price_max)
+        if search:
+            queryset = queryset.filter(Q(name__icontains=search) | Q(description__icontains=search))
+
+
+        return queryset
