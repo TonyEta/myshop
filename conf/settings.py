65b366e8c2fc11e9060721fc4b79a16cca3b2469
TontEta <shamruk01@gmail.com>
2026-07-16 21:12:27 +0300

    change docker-compose config

--- conf/settings.py
+++ conf/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'my_shop',
         'USER': 'jafar',
         'PASSWORD': 'qwerty25',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '5432'
     }
 }
